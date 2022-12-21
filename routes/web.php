bdb31e78f2a7deee6cc1fdf6fd003a98f2ba353d
Rehan2405 <117308871+Rehan2405@users.noreply.github.com>
2022-12-21 20:37:30 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/home', function () {
     return view('home');
 });
-
-Route::get('/about', function () {
-    return view('about');
+Route::get('/login', function () {
+    return view('login');
 });
+
+Auth::routes();
+
+// Route::get('/login', [App\Http\Controllers\HomeController::class, 'index'])->name('login');
+
+// Auth::routes();
+
+// Route::get('/login', [App\Http\Controllers\HomeController::class, 'index'])->name('login');
+
+// Google login
+Route::get('login/google', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
+
+// Facebook login
+Route::get('login/facebook', [App\Http\Controllers\Auth\LoginController::class, 'redirectToFacebook'])->name('login.facebook');
+Route::get('login/facebook/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleFacebookCallback']);
+
+// Github login
+Route::get('login/github', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGithub'])->name('login.github');
+Route::get('login/github/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGithubCallback']);
